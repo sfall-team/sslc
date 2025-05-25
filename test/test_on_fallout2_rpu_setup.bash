440a94a0db5320556234e41da5aa2a770038af56
Vasilii Rogin <vasya@rogin.ru>
2025-05-25 16:40:29 +0300

    Fix for different caches

--- test/test_on_fallout2_rpu_setup.bash
+++ test/test_on_fallout2_rpu_setup.bash
@@ -45,8 +45,12 @@ MODDERPACK_DIR=$(pwd)/modderspack
 ## Download modderspack
 if [ ! -d 'modderspack' ]; then
   echo "== modderpack =="
-  sudo apt update
-  sudo apt install -y p7zip-full
+
+  if ! which 7z >/dev/null; then
+    sudo apt update
+    sudo apt install -y p7zip-full
+  fi
+
 
   curl -L https://cyfuture.dl.sourceforge.net/project/sfall/Modders%20pack/modderspack_4.4.6.7z?viasf=1 > modderspack_4.4.6.7z
   7z x modderspack_4.4.6.7z -omodderspack
@@ -99,7 +103,7 @@ for f in $(find . -type f -iname '*.ssl') ; do
       echo "$DIR/$FNAME: building reference snapshot"
       set +e
 
-      if [ ! -n "$WINE_IS_INSTALLED" ]; then
+      if [ ! -n "$WINE_IS_INSTALLED" && -n "$WINE" ]; then
         echo "Installing wine"
         sudo dpkg --add-architecture i386
         sudo apt update
@@ -107,7 +111,7 @@ for f in $(find . -type f -iname '*.ssl') ; do
         WINE_IS_INSTALLED="yes"
       fi
 
-      wine $MODDERPACK_DIR/ScriptEditor/resources/compile.exe $SSLC_FLAGS \
+      $WINE $MODDERPACK_DIR/ScriptEditor/resources/compile.exe $SSLC_FLAGS \
         "-I$MODDERPACK_DIR/scripting_docs/headers" \
         "$FNAME" -o "$FBASE.int.expected" > "$FBASE.stdout.expected"
       RETURN_CODE_EXPECTED=$?
