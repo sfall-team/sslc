4f413e3ed58c5fc6e933e86bba924fbe010c60fb
Vasilii Rogin <vasya@rogin.ru>
2025-05-27 21:29:03 +0300

    Remove useless assignment

--- test/test_on_fallout2_rpu_run.bash
+++ test/test_on_fallout2_rpu_run.bash
@@ -16,9 +16,6 @@ cd gamescripts
 MODDERPACK_DIR=$(pwd)/modderspack
 
 
-## TODO: They should be the same as in snapshot build
-SSLC_FLAGS="-q -p -l -O2 -d -s -n"
-
 cd Fallout2_Restoration_Project/scripts_src
 
 run_tests "$SSLC" "$MODDERPACK_DIR/scripting_docs/headers"
