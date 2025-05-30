860613a178f17a23aac69aff97479cf9ccfdcac6
Vasilii Rogin <vasya@rogin.ru>
2025-05-30 17:02:27 +0300

    Rename true/false variables into is_true/is_false

--- gencode.c
+++ gencode.c
@@ -662,9 +662,9 @@ static int writeStatement(NodeList *n, int i, FILE *f) {
 			break;
 		}
 		case T_IF: {
-			int true, false, j;
+			int is_true, is_false, j;
 
-			false = outputTell(f);
+			is_false = outputTell(f);
 			writeInt(0, f);
 			j = i;
 			i = writeExpression(n, i+1, f);
@@ -682,11 +682,11 @@ static int writeStatement(NodeList *n, int i, FILE *f) {
 			else i = writeStatement(n, i, f);
 
 			if (n->nodes[i].token == T_ELSE) {
-				true = outputTell(f);
+				is_true = outputTell(f);
 				writeInt(0, f);
 				writeOp(O_JMP, f);
 
-				patchOffset(false+OPCODE_SIZE, outputTell(f), f);
+				patchOffset(is_false+OPCODE_SIZE, outputTell(f), f);
 
 				i++;
 				if (n->nodes[i].token == T_BEGIN) {
@@ -696,19 +696,19 @@ static int writeStatement(NodeList *n, int i, FILE *f) {
 					i++;
 				}
 				else i = writeStatement(n, i, f);
-				patchOffset(true+OPCODE_SIZE, outputTell(f), f);
+				patchOffset(is_true+OPCODE_SIZE, outputTell(f), f);
 			}
 			else {
 				unsigned long a = outputTell(f);
-				patchOffset(false+OPCODE_SIZE, a, f);
+				patchOffset(is_false+OPCODE_SIZE, a, f);
 			}
 
 			break;
 		}
 		case T_WHILE: {
-			int false, top, j, pos;
+			int is_false, top, j, pos;
 
-			false = outputTell(f);
+			is_false = outputTell(f);
 			writeInt(0, f);
 			top = outputTell(f);
 			loopStack[++loopStackPos].startPos = top;
@@ -732,7 +732,7 @@ static int writeStatement(NodeList *n, int i, FILE *f) {
 			writeOp(O_JMP, f);
 
 			pos = outputTell(f);
-			patchOffset(false+OPCODE_SIZE, pos, f);
+			patchOffset(is_false+OPCODE_SIZE, pos, f);
 
 			for (j = 0; j < loopStack[loopStackPos].numBreaks; j++) { // for each break, change it's JMP argument to proper address
 				patchOffset(breakStack[breakStackPos--] + OPCODE_SIZE, pos, f);
